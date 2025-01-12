e9d59b6894ccceeca58c99bc789893d192e9b4b3
vladelaina <vladelaina@gmail.com>
2025-01-12 22:42:00 +0800

    v1.17.2 optimization

--- src/window2/main.c
+++ src/window2/main.c
@@ -11,17 +11,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
 
+    // 设置OpenGL属性
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16); // 16x MSAA
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    
     // 设置纹理的缩放质量为最佳
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
 
-    // 创建窗口，设置为无边框
+    // 创建窗口，设置为无边框，添加OpenGL支持
     SDL_Window* window = SDL_CreateWindow(
         "Transparent Window",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         800, // 窗口宽度
         600, // 窗口高度
-        SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS
+        SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL
     );
 
     // 创建渲染器，启用透明支持和垂直同步
@@ -31,6 +37,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
     );
 
+    // 启用混合模式以支持透明度
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
     // 加载背景图片
     SDL_Surface* surface = IMG_Load("Background.png");
     if (!surface) {
@@ -38,13 +47,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
-    // 创建纹理
+    // 创建纹理并设置混合模式
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
     SDL_FreeSurface(surface);
 
     // 设置窗口透明
     SDL_SetWindowHitTest(window, NULL, NULL);
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
     // 主循环
     SDL_Event event;
