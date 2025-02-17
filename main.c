1d8fdbbf2e4145c267dbdefac1fd273719fe03dc
vladelaina <vladelaina@gmail.com>
2025-02-17 13:54:27 +0800

    v1.4.22 Removed extra suboptions

--- main.c
+++ main.c
@@ -1868,23 +1868,11 @@ void ShowColorMenu(HWND hwnd) {
                 GetLocalizedString(L"正计时", L"Stopwatch"));
     
     // 新增子选项：显示当前时间
-    AppendMenuW(hShowCurrentTimeSubMenu, MF_STRING | (strcmp(currentStartupMode, "SHOW_TIME") == 0 ? MF_CHECKED : 0),
+    AppendMenuW(hStartupSettingsMenu, MF_STRING | 
+                (strcmp(currentStartupMode, "SHOW_TIME") == 0 ? MF_CHECKED : 0),
                 CLOCK_IDC_START_SHOW_TIME,
-                GetLocalizedString(L"启动时显示", L"Show on Startup"));
-
-    AppendMenuW(hShowCurrentTimeSubMenu, MF_SEPARATOR, 0, NULL);
-
-    AppendMenuW(hShowCurrentTimeSubMenu, MF_STRING | (CLOCK_USE_24HOUR ? MF_CHECKED : MF_UNCHECKED),
-                CLOCK_IDM_24HOUR_FORMAT,
-                GetLocalizedString(L"24小时制", L"24-Hour Format"));
-    AppendMenuW(hShowCurrentTimeSubMenu, MF_STRING | (CLOCK_SHOW_SECONDS ? MF_CHECKED : MF_UNCHECKED),
-                CLOCK_IDM_SHOW_SECONDS,
-                GetLocalizedString(L"显示秒数", L"Show Seconds"));
-    
-    AppendMenuW(hStartupSettingsMenu, MF_POPUP,
-                (UINT_PTR)hShowCurrentTimeSubMenu,
                 GetLocalizedString(L"显示当前时间", L"Show Current Time"));
-
+    
     // 新增子选项：不显示
     AppendMenuW(hStartupSettingsMenu, MF_STRING | 
                 (strcmp(currentStartupMode, "NO_DISPLAY") == 0 ? MF_CHECKED : 0),
