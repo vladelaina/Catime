a39113a21abba491930554dee44bbc8efcc75461
vladelaina <vladelaina@gmail.com>
2025-03-05 21:09:17 +0800

    optimization

--- resource/resource.h
+++ resource/resource.h
@@ -221,7 +221,7 @@
 #define ABOUT_ICON_SIZE 128
 
 // 新增：关于对话框文本
-#define IDC_ABOUT_TITLE L"关于 Catime"
+#define IDC_ABOUT_TITLE L"Catime"
 #define IDC_ABOUT_VERSION L"版本: "
 #define IDC_ABOUT_OK_TEXT L"确定"
 #define IDC_ABOUT_CATIME L"Catime"
