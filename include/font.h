2ade084a90a3084da4e82c0819340659262885af
vladelaina <vladelaina@gmail.com>
2025-08-21 00:55:03 +0800

    docs: completed commenting on .h

--- include/font.h
+++ include/font.h
@@ -1,45 +1,107 @@
+/**
+ * @file font.h
+ * @brief Font management and preview system
+ * 
+ * Functions for loading, previewing, and managing application fonts
+ */
+
 #ifndef FONT_H
 #define FONT_H
 
 #include <windows.h>
 #include <stdbool.h>
 
+/**
+ * @brief Font enumeration callback procedure
+ * @param lpelfe Font enumeration data
+ * @param lpntme Text metrics data
+ * @param FontType Font type flags
+ * @param lParam User-defined parameter
+ * @return Callback processing result
+ */
 int CALLBACK EnumFontFamExProc(ENUMLOGFONTEXW *lpelfe, NEWTEXTMETRICEX *lpntme, DWORD FontType, LPARAM lParam);
 
+/**
+ * @brief Font resource information structure
+ */
 typedef struct {
-    int menuId;
-    int resourceId;
-    const char* fontName;
+    int menuId;            /**< Menu item identifier */
+    int resourceId;        /**< Resource identifier */
+    const char* fontName;  /**< Font file name */
 } FontResource;
 
+/** @brief Array of available font resources */
 extern FontResource fontResources[];
 
+/** @brief Count of available font resources */
 extern const int FONT_RESOURCES_COUNT;
 
+/** @brief Current font file name */
 extern char FONT_FILE_NAME[100];
 
+/** @brief Current font internal name */
 extern char FONT_INTERNAL_NAME[100];
 
+/** @brief Preview font file name */
 extern char PREVIEW_FONT_NAME[100];
 
+/** @brief Preview font internal name */
 extern char PREVIEW_INTERNAL_NAME[100];
 
+/** @brief Font preview active state */
 extern BOOL IS_PREVIEWING;
 
+/**
+ * @brief Load font from application resources
+ * @param hInstance Application instance handle
+ * @param resourceId Font resource identifier
+ * @return TRUE on success, FALSE on failure
+ */
 BOOL LoadFontFromResource(HINSTANCE hInstance, int resourceId);
 
+/**
+ * @brief Load font by file name
+ * @param hInstance Application instance handle
+ * @param fontName Font file name to load
+ * @return TRUE on success, FALSE on failure
+ */
 BOOL LoadFontByName(HINSTANCE hInstance, const char* fontName);
 
+/**
+ * @brief Write font configuration to settings
+ * @param font_file_name Font file name to save
+ */
 void WriteConfigFont(const char* font_file_name);
 
+/**
+ * @brief List all available system fonts
+ */
 void ListAvailableFonts(void);
 
+/**
+ * @brief Start font preview mode
+ * @param hInstance Application instance handle
+ * @param fontName Font name to preview
+ * @return TRUE on success, FALSE on failure
+ */
 BOOL PreviewFont(HINSTANCE hInstance, const char* fontName);
 
+/**
+ * @brief Cancel current font preview
+ */
 void CancelFontPreview(void);
 
+/**
+ * @brief Apply current preview font as active font
+ */
 void ApplyFontPreview(void);
 
+/**
+ * @brief Switch to different font immediately
+ * @param hInstance Application instance handle
+ * @param fontName Font name to switch to
+ * @return TRUE on success, FALSE on failure
+ */
 BOOL SwitchFont(HINSTANCE hInstance, const char* fontName);
 
 #endif
