e0484b26a37041ceb5df6b4577b3bc2d444fdb1f
vladelaina <vladelaina@gmail.com>
2026-08-23 20:38:46 +0800

    change version 1.6.0

--- resource/resource.h
+++ resource/resource.h
@@ -6,7 +6,7 @@
 #ifndef CATIME_RESOURCE_H
 #define CATIME_RESOURCE_H
 
-#define CATIME_VERSION "1.6.0-alpha4"
+#define CATIME_VERSION "1.6.0"
 
 /* Kept in the aggregate header for the embedded-resource packaging tool. */
 #define IDR_COMPRESSED_ASSETS 1200
